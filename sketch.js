e923ac5b5cd8f793595bb77e62320086481cc88a
affif443 <80408741+affif443@users.noreply.github.com>
2021-05-21 17:45:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var car,speed;
+var weight,wall;
+
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+
+  car = createSprite(50,200,50,50);
+  wall= createSprite(1200,200,60,100);
+
+  speed=random(40,90);
+  weight=random(500,1500);
+
+  car.velocityX=speed;
+}
+
+function draw() {
+  background(255,255,255);
+  
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  car.velocityX=0;
+  var deformation=0.5*speed*weight*speed/22509;
+  if(deformation > 180){
+    car.shapecolor="red";
+  }
+  if(deformation<180 && deformation>100){
+    car.shapecolor="yellow";
+  }
+  if(deformation < 100){
+    car.shapecolor="green";
+  }
+
+
+
+  drawSprites();
+}
